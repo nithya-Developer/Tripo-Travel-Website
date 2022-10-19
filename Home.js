ba3b34f82f68b6a1945f1ec265bf68c329ef3f92
nithya-Developer <nithi.nithyab@gmail.com>
2022-10-19 19:37:38 +0530

    added darktheme

--- Home.js
+++ Home.js
@@ -7,3 +7,10 @@ function closeCoupon() {
   document.getElementById("coupon").style.visibility = "hidden";
   document.getElementById("main").style.opacity = "1";
 }
+// ==== Dark theme =====
+
+
+function changeMode() {
+  let mybody = document.body;
+  mybody.classList.toggle("mydark");
+}
